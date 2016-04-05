c2f9c2b31f8760c725fa45b56b256a69fc6dac81
Derk van der Heide <derkvanderheide@hotmail.com>
2016-04-05 23:57:38 +0200

    added HttpKernel Controller Resolver

--- public/index.php
+++ public/index.php
@@ -5,9 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
 $request = Request::createFromGlobals();
 
@@ -16,19 +17,23 @@ $routes = include __DIR__ . '/../src/routes.php';
 $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
+$resolver = new ControllerResolver();
 
 try {
 	$request->attributes->add($matcher->match($request->getPathInfo()));
-	$response = call_user_func($request->attributes->get('_controller', 'render_template'), $request);
+	$controller = $resolver->getController($request);
+	$arguments = $resolver->getArguments($request, $controller);
+	$response = call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $e) {
 	$response = new Response('Not Found', 404);
 } catch (Exception $e) {
+	var_dump($e);
 	$response = new Response('An error occured', 500);
 }
 
 $response->send();
 
-function render_template($request) 
+function render_template(Request $request) 
 {
 	extract($request->attributes->all(), EXTR_SKIP);
 	ob_start();
--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,6 @@
 
 use Symfony\Component\Routing;
 use Symfony\Component\Routing\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 function is_leap_year($year = null) {
 	if($year === null) {
@@ -14,16 +13,10 @@ function is_leap_year($year = null) {
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', ['name' => 'World', '_controller' => 'render_template']));
-$routes->add('bye', new Route('/bye'));
+$routes->add('bye', new Route('/bye', ['_controller' => 'render_template']));
 $routes->add('leap_year', new Route('/is_leap_year/{year}', [
 	'year' => null,
-	'_controller' => function($request) {
-		if(is_leap_year($request->attributes->get('year'))) {
-			return new Response('Yep, this is a leap year!');
-		}
-
-		return new Response('Nope, this is not a leap year.');
-	}
+	'_controller' => 'Controllers\LeapYearController::indexAction'
 ]));
 
 return $routes;
