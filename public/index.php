9ecfdd9fe39d96df70cd02188a124396b926f3c3
Derk van der Heide <derkvanderheide@hotmail.com>
2016-04-05 21:14:47 +0200

    Added controller

--- public/index.php
+++ public/index.php
@@ -18,11 +18,8 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-	extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-	ob_start();
-	include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-	$response = new Response(ob_get_clean());
+	$request->attributes->add($matcher->match($request->getPathInfo()));
+	$response = call_user_func($request->attributes->get('_controller', 'render_template'), $request);
 } catch (ResourceNotFoundException $e) {
 	$response = new Response('Not Found', 404);
 } catch (Exception $e) {
@@ -30,3 +27,12 @@ try {
 }
 
 $response->send();
+
+function render_template($request) 
+{
+	extract($request->attributes->all(), EXTR_SKIP);
+	ob_start();
+	include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+	return new Response(ob_get_clean());
+}
